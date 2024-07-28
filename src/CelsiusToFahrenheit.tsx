64054d63af244c16bf28b9bb8ccbe0a1a38c636f
Vivek Indlebele Narasimha Prasad <vindlebe@asu.edu>
2024-07-28 11:20:34 -0400

    Added functionlity for celcius to fahrenheit

---
+++ src/CelsiusToFahrenheit.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import "./Celcius.Farenheit.css";
+
+const CelsiusToFahrenheit: React.FC = () => {
+  const [celsius, setCelsius] = useState<number | string>('');
+  const [fahrenheit, setFahrenheit] = useState<number | string>('');
+
+  const handleCelsiusChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setCelsius(value);
+
+    if (value === '') {
+      setFahrenheit('');
+    } else {
+      const celsiusValue = parseFloat(value);
+      setFahrenheit(((celsiusValue * 9) / 5 + 32).toFixed(2));
+    }
+  };
+
+  return (
+    <div>
+      <h1>Celsius to Fahrenheit Converter</h1>
+      <input
+        type="number"
+        value={celsius}
+        onChange={handleCelsiusChange}
+        placeholder="Enter Celsius"
+      />
+      <p>Fahrenheit: {fahrenheit}</p>
+    </div>
+  );
+};
+
+export default CelsiusToFahrenheit;
